511ba0676a4692d68734d8d063ece3ee32c4845a
rahadi rafsanjani <rafa@softwareseni.com>
2025-02-11 14:02:58 +0700

    chore: create database conection

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/rahadirafsanjani/notes_app/database"
 )
 
 func main() {
 	// Start a new fiber app
 	app := fiber.New()
+
+	// Connect to the Database
+	database.ConnectDB()
 
 	// Send a string back for GET calls to the endpoint "/"
 	app.Get("/", func(c *fiber.Ctx) error {
